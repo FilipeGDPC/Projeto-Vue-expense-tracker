db2f1534e168675095fca627bb68cf60d3ab9a2b
Filipe Cordeiro <filipegama2411@gmail.com>
2024-09-19 17:49:04 -0300

    Refactor Balance component to display the total balance dynamically

--- vue-expense-tracker/src/App.vue
+++ vue-expense-tracker/src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <Header />  
     <div class="container">
-    <Balance />
+    <Balance :total="total"/>
     <IncomeExpenses />
     <TransactionList :transactions="transactions" />
     <AddTransaction />
@@ -15,12 +15,16 @@ import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const transactions = ref([
             { id: 1, text: 'Flower', amount: -20 },
             { id: 2, text: 'Salary', amount: 300 },
             { id: 3, text: 'Book', amount: -10 },
             { id: 4, text: 'Camera', amount: 150 }
-        ]);       
+        ]);
+        
+const total = computed(() => {
+    return transactions.value.reduce((acc, transaction) => {return acc + transaction.amount}, 0);
+});
 </script>
