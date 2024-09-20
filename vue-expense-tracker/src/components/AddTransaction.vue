5d46e921dec323c466cfdd7aad510af892f9636d
Filipe Cordeiro <filipegama2411@gmail.com>
2024-09-20 17:17:44 -0300

    Update package dependencies to include vue-toastification

--- vue-expense-tracker/src/components/AddTransaction.vue
+++ vue-expense-tracker/src/components/AddTransaction.vue
@@ -1,6 +1,6 @@
 <template>
      <h3>Add new transaction</h3>
-      <form id="form"> @submit.prevent="onSubmit">
+      <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
           <label for="text">Text</label>
           <input type="text" id="text" v-model="text" placeholder="Enter text..." />
